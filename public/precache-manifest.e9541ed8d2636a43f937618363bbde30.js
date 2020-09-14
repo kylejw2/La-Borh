8fdd53f46c16994188b8a7927d6138018b58921e
Kyle Wright <kylejamwright@gmail.com>
2020-09-14 11:35:57 -0600

    Preparing application for Heroku

---
+++ public/precache-manifest.e9541ed8d2636a43f937618363bbde30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3a33490faf7190652c3725850179b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1547db909d6dba9e670c",
+    "url": "/static/css/main.3b45a64a.chunk.css"
+  },
+  {
+    "revision": "0800e4812c6a33e8f92d",
+    "url": "/static/js/2.b1653828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1653828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1547db909d6dba9e670c",
+    "url": "/static/js/main.b5a0bf41.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
